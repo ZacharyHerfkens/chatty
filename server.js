5f0e2acd0e3535b9a8745d47a66f08998239758e
Zachary Herfkens <zacharyherfkens1@gmail.com>
2023-03-20 12:39:48 -0700

    Moved server functions to class

--- server.js
+++ server.js
@@ -3,61 +3,68 @@ const mongoose = require("mongoose");
 
 const app = express();
 
+class Server {
+    static async init() {
+        let server = new Server();
+        await server._start();
+        return server;
+    }
 
-var chats = [];
+    async _start() {
+        log("Starting server...");
+        await mongoose.connect("mongodb://127.0.0.1/chatty");
+        const chatSchema = new mongoose.Schema({
+            time: Number,
+            alias: String,
+            message: String
+        });
+        this.Chat = mongoose.model("chat", chatSchema);
+        log("MongoDB initialized.");
 
-async function main() {
-    log("Server starting...");
-    await mongoose.connect("mongodb://127.0.0.1/assignment8", {
-        useNewUrlParser: true
-    });
-    const chatSchema = new mongoose.Schema({
-        time: Number,
-        alias: String,
-        message: String,
-    });
-    const Chat = mongoose.model("chat", chatSchema);
+        this.app = express();
+        this.app.use(express.static("public_html"));
+        this.app.use(express.json());
 
-    log("MongoDB connected.");
+        this.app.get("/chats", this._getChats.bind(this));
+        this.app.post("/chats/post", this._postChat.bind(this));
+        this.app.use(this._notFound.bind(this));
 
-    app.use(express.static("public_html"));
+        this.app.listen(80);
+        log("Server started: http://localhost");
+    }
 
-    app.get("/chats", async (req, res) => {
+    async _getChats(req, res) {
         log(`GET /chats [${req.ip}]`);
-        let chats = await Chat.find();
+        let chats = await this.Chat.find();
         chats.sort((a, b) => a.time - b.time);
         res.json(chats);
-    });
+    }
 
-    app.post("/chats/post",
-        express.json(),
-        async (req, res) => {
-            let { alias, message } = req.body;
-            if (!alias || !message) {
-                res.status(400).send("Bad Request");
-                err(`Bad request: ${req.body}`)
-                return;
-            }
-            let time = Date.now();
-            try {
-                await Chat({
-                    time: time,
-                    alias,
-                    message
-                }).save();
-                res.status(200).send("OK");
-                log(`Chat posted: ${alias} [${time.toLocaleString()}]: ${message}}`)
-            } catch (err) {
-                err(`Error saving chat: ${err}`);
-                res.status(500).send("Internal Server Error");
-            }
-        });
+    async _postChat(req, res) {
+        let { alias, message } = req.body;
+        if (!alias || !message) {
+            res.status(400).send("Bad Request");
+            err(`Bad request: ${req.body}`)
+            return;
+        }
+        let time = Date.now();
+        try {
+            await this.Chat({
+                time: time,
+                alias,
+                message
+            }).save();
+            res.status(200).send("OK");
+            log(`Chat posted: ${alias} [${new Date(time).toLocaleString()}]: ${message}`)
+        } catch (err) {
+            err(`Error saving chat: ${err}`);
+            res.status(500).send("Internal Server Error");
+        }
+    }
 
-    app.use((req, res) => {
+    _notFound(req, res) {
         res.status(404).send("404 Not Found");
-    });
-    app.listen(80);
-    log("Server started: http://localhost");
+    }
 }
 
 function log(msg) {
@@ -68,4 +75,4 @@ function err(msg) {
     console.error(`[${new Date().toLocaleString()}] | ${msg}`);
 }
 
-main().catch(err);
+Server.init();
